068804b7c495899d765465a0448a4cf40d7164d8
Kévin <kevin.lacroix@hetic.net>
2021-06-21 17:42:09 +0200

    Fonctionnalité terminé

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const[query, setQuery] = useState('');
+
+  const[weather, setWeather] = useState({});
+
+  const search =  async(e) => {
+    if(e.key === 'Enter') {
+      const data = await fetchWeather(query)
+      setWeather(data);
+      setQuery('');
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input 
+        type="text"
+        className ="search"
+        placeholder = "Search..."
+        value={query}
+        onChange={ (e) => setQuery(e.target.value) }
+        onKeyPress = {search}
+      />
+
+      { weather.main && (
+
+        <div className="city">
+
+          <h2 className='city-name'>
+            <span>{ weather.name }</span>
+            <sup>{ weather.sys.country }</sup>
+          </h2>
+
+          <div className="city-temp">
+           { Math.round(weather.main.temp) }<sup>&deg;C</sup>
+            { Math.round(weather.main.temp) < 15
+            ? <p className="Froid">Il fait froid</p>
+            : <p className="Chaud">Il fait chaud</p>
+            }
+          </div>
+
+        </div>
+      )}
     </div>
   );
 }
